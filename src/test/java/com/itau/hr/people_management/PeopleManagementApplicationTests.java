37dba924de539df4806ec4c175c8a14cc30d14ea
JP Roberto <joaopsroberto@gmail.com>
2025-07-25 23:37:49 -0300

    Commit inicial

---
+++ src/test/java/com/itau/hr/people_management/PeopleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.hr.people_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
